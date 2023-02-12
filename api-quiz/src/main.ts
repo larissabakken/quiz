1b1710b38fb217840bb403b0c78cb937bb61e7e4
Larissa Ramos <devlarissasr@gmail.com>
2023-02-12 17:07:59 -0300

    feat: creating frontend using vuejs + tailwind

--- api-quiz/src/main.ts
+++ api-quiz/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3003, () => {
     console.log('Server is running on http://localhost:3003');
   });
